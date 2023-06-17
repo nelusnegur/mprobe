1d3a92d183c8208557a3e22d7539ac0a43ce353e
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2023-06-17 18:07:07 +0200

    Implement a raw chart data writer

--- crates/neovue/src/render.rs
+++ crates/neovue/src/render.rs
@@ -1,6 +1,7 @@
+mod chart;
 pub mod error;
 pub mod output;
-pub mod page;
+pub mod view;
 
 use crate::render::error::RenderError;
 
@@ -12,4 +13,33 @@ pub trait Render {
     fn render<O>(&self, output: &mut O) -> Result<(), RenderError>
     where
         O: OutputStream;
+}
+
+pub trait ChartStream {
+    fn set<I>(&mut self, iter: I) -> Result<(), std::io::Error>
+    where
+        I: IntoIterator<Item = DataItem>;
+
+    fn chunks<I, C>(&mut self, chunks_iter: I) -> Result<(), std::io::Error>
+    where
+        C: Iterator<Item = DataItem>,
+        I: Iterator<Item = C>;
+}
+
+pub trait DataWriter {
+    fn start(&mut self) -> Result<(), std::io::Error>;
+    fn write(&mut self, data: DataItem) -> Result<(), std::io::Error>;
+    fn end(self) -> Result<(), std::io::Error>;
+}
+
+#[derive(Debug)]
+pub struct DataItem {
+    x: f64,
+    y: f64,
+}
+
+impl DataItem {
+    pub fn new(x: f64, y: f64) -> DataItem {
+        Self { x, y }
+    }
 }
